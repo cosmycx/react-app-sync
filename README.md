f995fc8d2456b855c876d2b99a5f91e184f633c6
Cosmin Potocean <cosmycx@gmail.com>
2018-12-17 16:41:26 -0500

    first

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # react-app-sync
 Share and sync code between create-react-apps
+
+sync: shared <-> cra1/src/shared
+sync: shared <-> cra2/src/shared
+
+```
+.
+└───shared
+└───cra1
+    └───src
+        └───shared
+└───cra2
+    └───src
+        └───shared
+```
+
+each can be own git repo: cra1, cra2, and shared.
+
+example:
+```
---
+++ sync.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const dirname = __dirname
+
+const action = process.argv[2],
+      intSrcDir = process.argv[3] || 'shared',
+      extAppDir = process.argv[4] || 'shared'
+
+const ACTION_EXPORT = 'export', ACTION_IMPORT = 'import'
+
+if (action !== ACTION_EXPORT && action !== ACTION_IMPORT) {
+  console.log('action parameter not available')
+  process.exit(1)
+}// .guard ACTION_EXPORT or ACTION_IMPORT
+
+const intPath = `${dirname}/src/${intSrcDir}/`
+console.log('internal folder:', `/src/${intSrcDir}/`)
+
+const extPath = path.join(__dirname, '..' , extAppDir)
+console.log('external folder:', extPath)
+
+const { spawn } = require('child_process')
+let rsync
+
+switch (action) {
+  case ACTION_IMPORT:
+    rsync = spawn( 'rsync', [ '-avu', extPath, intPath] )
+    break
+  default: //ACTION_EXPORT
+    rsync = spawn( 'rsync', [ '-avu', intPath, extPath] )
+}// .switch
+
+// rsync.stdout.on( 'data', data => console.log(`stdout: ${data}`) )
+
+rsync.stderr.on( 'data', data => console.log( `stderr: ${data}`) )
+
+rsync.on( 'close', code => {
+  if (code === 0) {
+    console.log(`sync ${action} OK`)
+  } else {
+    console.log(`sync process err, exited with code ${code}`)
+  }
+} )// .rsync on close
